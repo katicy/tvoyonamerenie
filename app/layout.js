e6f4386d511f08dc9cce2ca7b336c59e71dc90f8
katicy <katefo631@gmail.com>
2026-07-24 11:10:17 +0400

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
         <Nav />
         <main style={{ flex: 1 }}>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
